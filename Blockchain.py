c54340fe6e34a10f1162aa0a6e1bf493e235780d
Akshay Gupta <gupta.akshay01@gmail.com>
2019-04-05 00:32:14 -0400

    Basic Functionality

--- Blockchain.py
+++ Blockchain.py
@@ -1,22 +1,59 @@
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_trans = []
 
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         # Creates a new block and adds it to the chain
-        pass
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'transaction': self.current_trans,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
 
-    def new_trans(self):
+        # Reset the current  transaction list
+        self.current_trans = []
+
+        self.chain.append(block)
+        return block
+
+    def new_trans(self, sender, receiver, amount):
         # Adds a new transaction to the list of transactions
-        pass
+        self.current_trans.append({
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
 
     @staticmethod
-    def hash(self):
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
+
+    @staticmethod
+    def hash(self, block):
         # Hashes a Block
-        pass
+        block_string = json.dump(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the list block in the chain
-        pass
+        return self.chain[-1]
