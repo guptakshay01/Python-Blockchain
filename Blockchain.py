6872843b524a7700144e24f3d47da1c28a8ee415
Akshay Gupta <gupta.akshay01@gmail.com>
2019-04-04 21:23:19 -0400

    Class Skeleton

---
+++ Blockchain.py
@@ -0,0 +1,22 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_trans = []
+
+    def new_block(self):
+        # Creates a new block and adds it to the chain
+        pass
+
+    def new_trans(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(self):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the list block in the chain
+        pass
